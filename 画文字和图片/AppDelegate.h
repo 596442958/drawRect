3e2187fc111fd3fbe97ef4c89a7b65c9a01b73a4
Miles <596442958@qq.com>
2016-02-09 14:44:34 +0800

    Initial Commit

---
+++ 画文字和图片/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  画文字和图片
+//
+//  Created by Miles on 2/9/16.
+//  Copyright © 2016 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 画文字和图片/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  画文字和图片
+//
+//  Created by Miles on 2/9/16.
+//  Copyright © 2016 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
